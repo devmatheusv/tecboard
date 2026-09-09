a9381e8f5d248f81845cf5bd0f4e4551cadefb5d
MatheusValino <devmatheusv@gmail.com>
2026-09-08 21:44:31 -0300

    criando componente tema e o seu estilo.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,63 @@
 import './App.css'
+import { Banner } from './componentes/Banner'
 import { FormularioDeEventos } from './componentes/FormularioDeEventos'
+import { Tema } from './componentes/Tema'
 // no react, componentes são FUNÇÕES
 
 function App() {
+
+  const temas = [
+    {
+      id: 1,
+      nome: 'front-end'
+    },
+    {
+      id: 2,
+      nome: 'back-end'
+    },
+    {
+      id: 3,
+      nome: 'devops'
+    },
+    {
+      id: 4,
+      nome: 'inteligência artificial'
+    },
+    {
+      id: 5,
+      nome: 'data science'
+    },
+    {
+      id: 6,
+      nome: 'cloud'
+    },
+  ]
+
   return (
     <main>
       <header>
         <img src="/logo.png" alt="" />
       </header>
+      <Banner />
+      <FormularioDeEventos />
       <section>
-        <img src="/banner.png" alt="" />
+        <Tema tema={temas[0]} />
       </section>
-      <FormularioDeEventos  />
+      <section>
+        <Tema tema={temas[1]} />
+      </section>
+      <section>
+        <Tema tema={temas[2]} />
+      </section>
+      <section>
+        <Tema tema={temas[3]} />
+      </section>
+      <section>
+        <Tema tema={temas[4]} />
+      </section>
+      <section>
+        <Tema tema={temas[5]} />
+      </section>
     </main>
   )
 }
